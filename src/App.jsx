3e49b7f9eecb055e6db8159e28995db47e4be26c
Fallcon <fallcon@local.dev>
2026-03-15 02:21:18 +0000

    feat: auto scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -46,10 +46,21 @@ function SeoHandler() {
   return null
 }
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }, [pathname])
+
+  return null
+}
+
 export default function App() {
   return (
     <>
       <SeoHandler />
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
